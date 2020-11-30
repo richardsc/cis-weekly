836a5a357f2f347b7533f6fdbeb269b85a025692
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2020-11-30 16:34:31 -0400

    nc code to write all variables

--- nc-collect.R
+++ nc-collect.R
@@ -3,7 +3,81 @@ library(tidyverse)
 library(stars)
 library(ncdf4)
 
-n_ct_tif_files <- list.files("tif/n-ct", "\\.tif$", full.names = TRUE)
+# generate the mapping information
+
+e_value_mapping <- read_csv(
+  "tif/value_mapping.csv",
+  col_types = cols(
+    category = col_character(),
+    column_value = col_character(),
+    raster_value = col_double(),
+    quantitative_definition = col_character(),
+    definition = col_character(),
+    notes = col_character()
+  )
+) %>%
+  mutate(column_value = str_replace(column_value, '"unknown"', "unknown")) %>%
+  group_by(category) %>%
+  summarise(
+    mapping = glue::glue_collapse(
+      glue::glue('{{"raster_value": "{ raster_value }", "column_value": "{ column_value }", "quantitative_definition": "{ quantitative_definition }", "definition": "{ definition }"}}'),
+      sep = ", "
+    ) %>%
+      paste0("[", ., "]")
+  )
+
+# make sure mappings can be parsed as JSON
+walk(e_value_mapping$mapping, jsonlite::fromJSON)
+
+e_columns <- read_csv(
+  "tif/variables.csv",
+  col_types = cols(
+    category = col_character(),
+    column = col_character(),
+    symbol = col_character(),
+    definition = col_character(),
+    notes = col_character()
+  )
+) %>%
+  select(-notes) %>%
+  left_join(e_value_mapping, by = "category") %>%
+  mutate(long_name = glue::glue("{ symbol }: { definition }")) %>%
+  select(-category, - symbol, -definition) %>%
+  rename(units = mapping)
+
+e_cols <- c(
+  "E_CT", "E_CA", "E_CB", "E_CC", "E_CD", "E_SO", "E_SA", "E_SB",
+  "E_SC", "E_SD", "E_SE", "E_FA", "E_FB", "E_FC", "E_FD", "E_FE"
+)
+
+n_columns <- tibble(
+  column = c(
+    "N_CT", "N_COI", "N_CMY", "N_CSY", "N_CFY", "N_CFY_TK", "N_CFY_M",
+    "N_CFY_TN", "N_CYI", "N_CGW", "N_CG", "N_CN", "N_CB", "N_CVTK",
+    "N_CTK", "N_CM", "N_CTN"
+  ),
+  long_name = column,
+  units = "Original column value * 10"
+)
+
+columns <- bind_rows(e_columns, n_columns)
+
+files <- columns %>%
+  select(column) %>%
+  mutate(
+    file = column %>%
+      str_to_lower() %>%
+      str_replace_all("_", "-") %>%
+      file.path("tif", .) %>%
+      lapply(list.files, "\\.tif$", full.names = TRUE)
+  ) %>%
+  unnest(file) %>%
+  mutate(
+    region = str_sub(basename(file), 1, 2),
+    date = as.Date(str_extract(basename(file), "[0-9-]{10}"))
+  )
+
+# used for land mask
 pnt_type_tif_files <- list.files("tif/pnt-type", "\\.tif$", full.names = TRUE)
 
 land_mask_region <- function(region, using_n_files = 100) {
@@ -30,12 +104,36 @@ land_mask_region <- function(region, using_n_files = 100) {
   tif0
 }
 
-region_to_netcdf <- function(region) {
-  n_ct_tif_region <- n_ct_tif_files[str_starts(basename(n_ct_tif_files), region)]
-  dates_region <- as.Date(str_extract(basename(n_ct_tif_region), "[0-9-]{10}"))
+region_to_netcdf <- function(region, pb = function(...) NULL) {
+  pb(
+    message = glue::glue("Preparing region '{ region }'"),
+    class = "sticky",
+    amount = 0
+  )
 
-  tif0 <- read_stars(n_ct_tif_region[1])
+  region_files <- files %>%
+    filter(region == !! region) %>%
+    select(-region)
+
+  # make sure variables/regions exist on a perfect grid
+  dates_region <- sort(unique(region_files$date))
+  vars_region <- sort(unique(region_files$column))
+  region_columns <- columns[match(vars_region, columns$column), ]
+
+  region_files_grid <- crossing(
+    tibble(column = vars_region),
+    tibble(date = dates_region)
+  ) %>%
+    arrange(column, date) %>%
+    left_join(region_files, by = c("column", "date"))
+
+  tif0 <- read_stars(glue::glue("tif/{ region }-template.tif"))
   crs_chr <- st_crs(tif0)$Wkt
+
+  # make empty tif that can be used as a fill if a date/variable combo
+  # is not available
+  tif_na <- tif0
+  tif_na[[1]][] <- NA_integer_
 
   coords <- crossing(
     tibble(
@@ -106,59 +204,88 @@ region_to_netcdf <- function(region) {
     compression = 5
   )
 
-  var_n_ct <- ncvar_def(
-    "n_ct",
-    units = "percent",
-    dim = list(dim_x, dim_y, dim_juld),
-    longname = "Total ice concentration",
-    missval = 255L,
-    prec = "integer",
-    compression = 5
-  )
+  vars_value <- region_columns %>%
+    rename(name = column, longname = long_name) %>%
+    pmap(
+      ncvar_def,
+      dim = list(dim_x, dim_y, dim_juld),
+      missval = 255L,
+      prec = "integer",
+      compression = 5
+    )
 
   # create file
   nc <- nc_create(
     glue::glue("nc/{ region }.nc"),
-    vars = list(var_crs, var_land, var_longitude, var_latitude, var_n_ct)
+    vars = c(
+      list(var_crs, var_land, var_longitude, var_latitude),
+      vars_value
+    )
   )
+
+  # always close even on error
+  on.exit(nc_close(nc))
 
   # this is how GDAL writes CRS info
   ncatt_put(nc, var_crs, "spatial_ref", crs_chr)
   ncatt_put(nc, var_land, "grid_mapping", "crs")
   ncatt_put(nc, var_longitude, "grid_mapping", "crs")
   ncatt_put(nc, var_latitude, "grid_mapping", "crs")
-  ncatt_put(nc, var_n_ct, "grid_mapping", "crs")
+  for (var in vars_value) {
+    ncatt_put(nc, var, "grid_mapping", "crs")
+  }
 
+  # put constant variables for each region
   ncvar_put(nc, var_land, vals = land_def[[1]])
   ncvar_put(nc, var_longitude, vals = coords$longitude)
   ncvar_put(nc, var_latitude, vals = coords$latitude)
 
+  pb(
+    message = glue::glue("Prepared region '{ region }'"),
+    class = "sticky",
+    amount = 0
+  )
+
   # write values one timestep at a time
-  pb <- progress::progress_bar$new(total = length(dates_region))
-  for (i in seq_along(dates_region)) {
-    pb$tick()
-    strs <- read_stars(n_ct_tif_region[i])
+  for (item in transpose(region_files_grid)) {
+    if (is.na(item$file)) {
+      strs <- tif_na
+    } else {
+      pb(message = item$file)
+      strs <- read_stars(item$file)
+    }
+
     vals <- strs[[1]]
     vals[is.na(vals)] <- 255L
 
     ncvar_put(
       nc,
-      var_n_ct,
+      vars_value[[which(map_chr(vars_value, "name") == item$column)]],
       vals,
-      start = c(1, 1, i),
+      start = c(1, 1, match(item$date, dates_region)),
       count = c(-1, -1, 1)
     )
   }
-
-  nc_close(nc)
 }
 
 if (!dir.exists("nc")) dir.create("nc")
 
 
-regions <- unique(str_extract(basename(n_ct_tif_files), "^[A-Z]{2}"))
+regions <- unique(str_extract(basename(files$file), "^[A-Z]{2}"))
 
 # can only paralellize here using regions because packing
 # each NetCDF file has to be done sequentially
 future::plan(future::multisession, workers = future::availableCores() - 1)
-furrr::future_walk(regions, region_to_netcdf, .progress = TRUE)
+
+progressr::handlers("progress")
+progressr::with_progress({
+  # initiate progressor! lets us specify one tick per file rather than
+  # one tick by region
+  pb <- progressr::progressor(steps = nrow(files))
+
+  furrr::future_walk(
+    regions,
+    region_to_netcdf,
+    pb = pb
+  )
+})
